505be882089b8833dfb07a4a265e6349dfaf81a1
Acácio <99909819+acaciobjr@users.noreply.github.com>
2024-02-07 14:31:40 -0300

    Update CMC.py

--- CMC.py
+++ CMC.py
@@ -35,48 +35,39 @@ tabela = []
 
 for moeda, quantidade in informacoes_usuario.items():
     
-    xpathBusca = ["//html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div/div[3]/div/div[2]/div[3]/div/div[1]",
-                  "//html/body/div[1]/div[2]/div/div[1]/div/div[1]/div/div[3]/div/div[2]/div[3]/div/div[1]"]
-    for xpath in xpathBusca:
-        try:            
-            pesquisa = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, xpath)))
-            pesquisa.click()
-            print('abrindo pesquisa')
-            time.sleep(4)            
-            break
-        except Exception as e:
-            print(f'1-botão de pesquisa não achado Erro: {str(e)}')
-            continue
-          
-    input = "//html/body/div[1]/div[2]/div/div[1]/div/div[1]/div/div[3]/div/div[2]/div[4]/div/div/div/div/div[1]/div[1]/div[1]"
-    print('procurando input pesquisa')
-    try:
+    xpathBusca = '//html/body/div[1]/div[2]/div/div[1]/div/div[1]/div/div[3]/div/div[2]/div[3]/div/div[1]'
+    xpathinput = "//html/body/div[1]/div[2]/div/div[1]/div/div[1]/div/div[3]/div/div[2]/div[4]/div/div/div/div/div[1]/div[1]/div[1]/input"
+    try:            
+        pesquisa = WebDriverWait(driver, 15).until(
+        EC.visibility_of_element_located((By.XPATH, xpathBusca)))
+        pesquisa.click()
+        print('abrindo pesquisa')
+        time.sleep(4) 
+        print('procurando input pesquisa')
         pesquisai = WebDriverWait(driver, 30).until(
-        EC.visibility_of_element_located((By.XPATH, input))
-        )
-        #pesquisai.click()
+        EC.visibility_of_element_located((By.XPATH, xpathinput)))
+        pesquisai.click()        
         time.sleep(5)
-        primeira_chave = moeda
-        print('tentando enviar pesquisa')
+        print(f'tentando enviar pesquisa: {moeda}')
         pesquisai.send_keys(moeda)
-    except Exception as m:
-        print(f'2-espaço de input não achado Erro: {str(m)}')
+    except Exception as e:
+        print(f'1-espaço input não achado Erro: {str(e)}')
         break
-    time.sleep(6)
+    
+    time.sleep(13)
     buscaResult = "//html/body/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div/div[3]/div/div[2]/div[4]/div/div/div/div/div[2]/div[2]/div[1]/a[1]/div"
-    pesquisa2 = WebDriverWait(driver, 15).until(
+    pesquisa2 = WebDriverWait(driver, 45).until(
     EC.visibility_of_element_located((By.XPATH, buscaResult))
     )
     print('tentando clicar moeda buscada')
     pesquisa2.click()
-    time.sleep(10)
+    time.sleep(8)
     try:
         print('buscando preço')
         imgPreco = "//*[@id='section-coin-overview']/div[2]/span"
-        imgPreco2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, imgPreco)))
+        imgPreco2 = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, imgPreco)))
         preco2 = imgPreco2.text
-        print(f'preço de {primeira_chave} é {preco2}')
+        print(f'preço de {moeda} é {preco2}')
     except StaleElementReferenceException:
         print('preço não achado')
 
@@ -84,16 +75,13 @@ for moeda, quantidade in informacoes_usuario.items():
     valor = preco2.replace('$', '')
     valorDolar = float(valor)
     montante = primeiraQuantidade * valorDolar
-    print(f'Seu montante para {primeira_chave} é de ${montante}')
-    moeda = primeira_chave
+    print(f'Seu montante para {moeda} é de ${montante}')
     quantidade = primeiraQuantidade
-    preço_em_dólar = valorDolar
     total_em_dólar = round(montante, 2)
     total_em_real = montante * taxa_dolar_real
-    linha_atual = [moeda, quantidade, preço_em_dólar, total_em_dólar, total_em_real]
+    linha_atual = [moeda, quantidade, valorDolar, total_em_dólar, total_em_real]
     tabela.append(linha_atual)
 
-#fora do for
 tabela2 = tabulate(tabela,headers=cabecalho,tablefmt="grid")
 print(tabela2)
 
