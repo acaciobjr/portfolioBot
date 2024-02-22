3ea998c73efd8debd5616128832098709c467bc0
Acácio <99909819+acaciobjr@users.noreply.github.com>
2024-02-22 11:33:16 -0300

    Update CMC.py

--- CMC.py
+++ CMC.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
-from forex_python.converter import CurrencyRates
 from tabulate import tabulate
 import time
 import os
